a3160d76170281c7001186ea3975b7d263ca194a
FathanRohman <doctor.fathan@gmail.com>
2016-08-17 20:31:23 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/* ---------------------------------
+Javascript(jQuery) for Microbiz
+
+version: 1.0
+author : Fathan Rohman
+email  : karir.fathan@gmail.com
+website: http://microbiz.co
+-----------------------------------*/
+
+$(document).ready(function () {
+	$('#header-microbiz').scrollToFixed();
+});
